86cbff23a62a390ed3323c9039d672e6885199a8
unknown <Professor@SN-351300.edu.fiesc.com.br>
2025-10-29 15:18:10 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+import Home from './screens/Home'
+
+export default function App() {
+  return (
+   <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name="Home" component={Home} options={{title: 'Início'}}/>
+
+      </Stack.Navigator>
+   </NavigationContainer>
+  );
+}
